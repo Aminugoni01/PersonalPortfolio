5decce68d26c9df285b08bc80de8d565e514b90b
Aminu Goni Bukar <aminugonibukar123@gmail.com>
2025-07-14 17:40:45 +0100

    Update job title to FULL-STACK Web Developer and correct casing in project description

--- src/components/projectsSection/ProjectsText.jsx
+++ src/components/projectsSection/ProjectsText.jsx
@@ -5,7 +5,7 @@ const ProjectsText = () => {
       <p className="text-lg text-center">
         I have worked on a variety of web development projects, ranging from
         responsive websites for small businesses to full-stack applications and
-        complex front-end interfaces.
+        complex Front-End interfaces.
       </p>
     </div>
   );
